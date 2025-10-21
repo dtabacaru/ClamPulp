e4e9107c2dfe2c0eb34b5a83cfb857dd27aeddbb
Danut Tabacaru <dtabacaru@gmail.com>
2025-10-21 14:18:39 -0400

    feat: re-work entire addon

--- ClamPulp.lua
+++ ClamPulp.lua
@@ -1,34 +1,41 @@
-local clamList = {
-	["Big-mouth Clam"] = true,
-	["Thick-shelled Clam"] = true,
-	["Small Barnacled Clam"] = true,
-	["Soft-shelled Clam"] = true,
-};
+local CLAM_LIST = {
+  [ 7973] = true, -- Big-mouth Clam
+  [ 5524] = true, -- Thick-shelled Clam
+  [ 5523] = true, -- Small Barnacled Clam
+  [15874] = true, -- Soft-shelled Clam
+}
 
-local clamFrame = CreateFrame('Frame');
-clamFrame:RegisterEvent("BAG_UPDATE")
-clamFrame:SetScript('OnEvent', function(self, event)
-	if not GetCVarBool("autoLootDefault") then
-		return
-	end
-	for i = 0, 4 do
-		local numSlots = C_Container.GetContainerNumSlots(i);
-		
-		for j = 1, numSlots do
-		    local containerInfo = C_Container.GetContainerItemInfo(i, j)
+local function HasFreeSlots()
+  local freeSlots = 0
+  for bagNum = 0, 4 do
+    local numFreeSlots = C_Container.GetContainerNumFreeSlots(bagNum) or 0
+    freeSlots = freeSlots + numFreeSlots
+  end
+  return freeSlots > 0
+end
 
-			local itemLink
-			if containerInfo then 
-			  itemLink = containerInfo.hyperlink;
-			end
-		
-			if itemLink then
-				local name = strsub(itemLink, strfind(itemLink, "%[") + 1, strfind(itemLink, "]") - 1);
-			
-				if clamList[name] then
-					C_Container.UseContainerItem(i, j);
-				end
-			end
-		end
-	end
+local function OpenClams()
+  for bagNum = 0, 4 do
+    local numSlots = C_Container.GetContainerNumSlots(bagNum) or 0
+    for slotNum = 1, numSlots do
+      local itemId = C_Container.GetContainerItemID(bagNum, slotNum)
+      if itemId and CLAM_LIST[itemId] then
+        C_Container.UseContainerItem(bagNum, slotNum)
+        return -- open only one per event; next event will handle more
+      end
+    end
+  end
+end
+
+local clamFrame = CreateFrame("Frame")
+clamFrame:RegisterEvent("BAG_UPDATE_DELAYED")
+
+clamFrame:SetScript("OnEvent", function(self, event)
+  if not GetCVarBool("autoLootDefault") then 
+    return 
+  end
+  
+  if HasFreeSlots() then
+    OpenClams()
+  end
 end)
